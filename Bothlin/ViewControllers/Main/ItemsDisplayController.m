aeb3dc3c46028b920c1de02a8b8f63fa7ca594a5
Michael Dales <michael@digitalflapjack.com>
2023-10-02 17:33:29 +0100

    Fix collection view frame

--- Bothlin/ViewControllers/Main/ItemsDisplayController.m
+++ Bothlin/ViewControllers/Main/ItemsDisplayController.m
@@ -20,15 +20,26 @@
 
 - (instancetype)initWithNibName:(NSNibName)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    self->_gridViewController = [[GridViewController alloc] initWithNibName: @"GridViewController" bundle: nil];
-    self->_singleViewController = [[SingleViewController alloc] initWithNibName: @"SingleViewController" bundle: nil];
+    if (nil != self) {
+        self->_gridViewController = [[GridViewController alloc] initWithNibName: @"GridViewController" bundle: nil];
+        self->_singleViewController = [[SingleViewController alloc] initWithNibName: @"SingleViewController" bundle: nil];
+    }
     return self;
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+
     [self addChildViewController: self.gridViewController];
+
+    [self.gridViewController.view setFrame: self.view.frame];
     [self.view addSubview: self.gridViewController.view];
+}
+
+- (void)viewDidLayout {
+    [super viewDidLayout];
+    // I seem to need to do this otherwise the view is the wronng size in the split view.
+    [self.gridViewController.view setFrame: self.view.frame];
 }
 
 - (void)reloadData {
